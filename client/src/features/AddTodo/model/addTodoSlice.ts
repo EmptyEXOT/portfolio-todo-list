6cfd2639ba25ae05a11698ac0321e6d937058cb6
EmptyEXOT <sadullaev.alim@mail.ru>
2024-03-05 11:55:54 +0300

    addTodo feature init

---
+++ client/src/features/AddTodo/model/addTodoSlice.ts
@@ -0,0 +1,26 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {TodoModel} from "@/entities/Todo/model/types";
+
+const initialState: TodoModel = {
+    header: '',
+    created: '',
+    isFinished: false,
+    deadline: '',
+    description: ''
+}
+
+const addTodoSlice = createSlice({
+    name: 'addTodo',
+    initialState,
+    reducers: {
+        setHeader: (state, action: PayloadAction<string>) => {
+            state.header = action.payload
+        },
+        setDescription: (state, action: PayloadAction<string>) => {
+            state.description = action.payload
+        }
+    }
+})
+
+export const {actions: addTodoActions} = addTodoSlice
+export const {reducer: addTodoReducer} = addTodoSlice
---
+++ client/src/features/AddTodo/model/selectors.ts
@@ -0,0 +1,3 @@
+import {RootState} from "@/shared/store/store";
+
+export const selectAddTodo = (store: RootState) => store.addTodoReducer

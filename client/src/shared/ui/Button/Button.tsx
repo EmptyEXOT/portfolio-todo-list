8c47fc630bcdb8a748d8f734e268085bf3054833
EmptyEXOT <sadullaev.alim@mail.ru>
2024-03-06 14:09:39 +0300

    button improvement

--- client/src/shared/ui/Button/Button.tsx
+++ client/src/shared/ui/Button/Button.tsx
@@ -2,22 +2,41 @@ import React, {ButtonHTMLAttributes, FC, ReactNode} from 'react';
 import classNames from "classnames";
 import cls from './Button.module.scss'
 
+export enum ButtonVariant {
+    Default = 'default',
+    Alert = 'alert',
+    Success = 'success',
+    Warning = 'warning',
+}
+
+export interface ButtonModifiers {
+    default?: boolean,
+    outline?: boolean,
+    filled?: boolean,
+    disabled?: boolean,
+    rounded?: boolean,
+    circle?: boolean,
+}
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
     className?: string;
-    border?: boolean;
-    fill?: boolean;
-    disabled?: boolean;
+    modifiers?: ButtonModifiers;
+    variant: ButtonVariant;
 }
 
-const Button: FC<ButtonProps> = ({children, className, border, fill, disabled, ...props}) => {
+const Button: FC<ButtonProps> = ({children, className, modifiers, variant, ...props}) => {
     return (
-        <button disabled={disabled} {...props}
-                className={classNames(cls.button, className, border
-                        ? 'border-black rounded border-solid' : '',
-                    fill
-                        ? 'bg-black text-white' : '',
-                    disabled ? 'bg-blue-900 opacity-50' : '')}>
+        <button disabled={modifiers?.disabled} {...props}
+                className={classNames(
+                    cls.button,
+                    cls[variant],
+                    className,
+                    modifiers?.outline && cls.border,
+                    modifiers?.filled && cls.filled,
+                    modifiers?.rounded && cls.rounded,
+                    modifiers?.circle && cls.circle,
+                    modifiers?.disabled && 'bg-blue-900 opacity-50')}>
             {children}
         </button>
     );

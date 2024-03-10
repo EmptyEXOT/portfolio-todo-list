39cde31ef5dbd02eba835552328a9bc33f18c783
EmptyEXOT <sadullaev.alim@mail.ru>
2024-03-11 00:26:09 +0300

    Edit todo state

--- client/src/shared/store/types.ts
+++ client/src/shared/store/types.ts
@@ -1,7 +1,9 @@
 import {TodoListSchema} from "@/entities/Todo/model/types";
 import {AddTodoModel} from "@/features/AddTodo/model/types";
+import {EditTodoModel} from "@/features/EditTodoById/model/types";
 
 export interface StateSchema {
     todos: TodoListSchema,
     newTodo: AddTodoModel,
+    editTodo: EditTodoModel,
 }

89db465f5f5bf66dd39784d073065528d1532c09
EmptyEXOT <sadullaev.alim@mail.ru>
2024-03-13 19:09:55 +0300

    Error page fix

--- client/src/pages/ErrorPage/ErrorPage.tsx
+++ client/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import {useRouteError} from "react-router-dom";
-import {Root} from "@/root";
 import classNames from "classnames";
 
 const ErrorPage = () => {
     const {status, message} = useRouteError() as { status?: number, message?: string }
 
     return (
-        <Root>
-            <div className={classNames('pt-16 container mx-auto px-4 md:pt-20')}>
-                <p>Sorry, an unexpected error has occurred.</p>
-                <p>
-                    <i>{status || message}</i>
-                </p>
-            </div>
-        </Root>
+        <div className={classNames('pt-16 container mx-auto px-4 md:pt-20')}>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+                <i>{status || message}</i>
+            </p>
+        </div>
     );
 };
 

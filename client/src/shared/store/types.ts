44afcb19ab1a685da31bf08442f5486822bf57d0
EmptyEXOT <sadullaev.alim@mail.ru>
2024-03-13 19:14:30 +0300

    login state

--- client/src/shared/store/types.ts
+++ client/src/shared/store/types.ts
@@ -1,9 +1,11 @@
 import {TodoListSchema} from "@/entities/Todo/model/types";
 import {AddTodoModel} from "@/features/AddTodo/model/types";
 import {EditTodoModel} from "@/features/EditTodoById/model/types";
+import {LoginModel} from "@/features/LoginByEmail/model/types";
 
 export interface StateSchema {
     todos: TodoListSchema,
     newTodo: AddTodoModel,
     editTodo: EditTodoModel,
+    login: LoginModel,
 }

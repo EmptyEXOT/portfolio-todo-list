9a4b711da9d84b2d2eade4b18b491a7abbab12de
EmptyEXOT <sadullaev.alim@mail.ru>
2024-03-13 19:11:28 +0300

    with private HOC

---
+++ client/src/shared/hocs/withPrivate/PrivateComponent.tsx
@@ -0,0 +1,9 @@
+import React, {ReactNode} from "react";
+import {Navigate, useLoaderData} from "react-router-dom";
+
+export const PrivateComponent = ({children, redirect}: {children: ReactNode, redirect: string}) => {
+    const res = useLoaderData();
+    return (
+        res ? children : <Navigate to={redirect}/>
+    )
+}
